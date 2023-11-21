b0834d8cf74e877c541a138e355fce3fa559af9e
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-11-21 12:34:11 +0300

    Done laba 1 task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] data = new int[5];
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < data.length; ++i) {
+            data[i] = scan.nextInt();
+        }
+
+        int[] mirror_data = new int[5];
+        for (int i = data.length - 1, j = 0; i >= 0; --i, ++j) {
+            mirror_data[j] = data[i];
+        }
+
+        for (int i = 0; i < data.length; ++i) {
+            System.out.print(mirror_data[i] + " ");
+        }
     }
 }
